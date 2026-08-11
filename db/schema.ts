d11db72bfd0af9a75aaac34cf8a53004351cc955
Daniyal Subhani <daniyalsubhani527@gmail.com>
2026-08-11 19:27:31 +0500

    chore: remove db files from auth branch

--- db/schema.ts
+++
@@ -1,9 +0,0 @@
-import { string } from 'drizzle-orm/cockroach-core/columns/string';
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
-
-export const usersTable = pgTable('users', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  userId: string().references().clerk(),
-  createdAt: Date().now(),
-  updatedAt: Date().update(),
-});
