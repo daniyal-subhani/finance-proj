6ea26df61178246ff60c58d83bba5c3fbf7779e3
Daniyal Subhani <daniyalsubhani527@gmail.com>
2026-08-12 03:51:15 +0500

    chore: update require user utility

---
+++ lib/auth/require-user.ts
@@ -0,0 +1,10 @@
+import { getCurrentUser } from './current-user';
+
+export async function requireUser() {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    throw new Error('Unauthorized');
+  }
+  return user;
+}
